8965fde28df5f8f9eff3d468abf735c856ed19af
JoeyGrimes <107405472+JoeyGrimes@users.noreply.github.com>
2022-07-01 19:32:29 -0700

    Filled the divs with imgs

--- js/main.js
+++ js/main.js
@@ -1,46 +1,60 @@
 /*----- constants -----*/
-//Need to have an array of 1-20 with 10 possible matches
-//Better to have one long array rather than 10 two number arrays. 
-//Need to figure out how I can group. Whether by ID or color & textContent
+//Set an array of 20 objects, 10 of them are identical 
+
+
 const board = [ 
-   {img: "https://st2.depositphotos.com/1803332/9245/v/950/depositphotos_92457980-stock-illustration-playing-cards-back.jpg"
-   {img: "https://st2.depositphotos.com/1803332/9245/v/950/depositphotos_92457980-stock-illustration-playing-cards-back.jpg"
-   , matched: false}, 
-   {img: 'imgurl2', matched: false}, 
-   {img: 'imgurl2', matched: false}, 
+   {img: "imgur1", matched: false},
+   {img: 'imgur1', matched: false}, 
+   {img: 'imgur2', matched: false}, 
+   {img: 'imgur2', matched: false}, 
+   {img: 'imgur3', matched: false}, 
+   {img: 'imgur3', matched: false}, 
+   {img: 'imgur4', matched: false},
+   {img: 'imgur4', matched: false},
+   {img: 'imgur5', matched: false},
+   {img: 'imgur5', matched: false},
+   {img: 'imgur6', matched: false}, 
+   {img: 'imgur6', matched: false}, 
+   {img: 'imgur7', matched: false}, 
+   {img: 'imgur7', matched: false}, 
+   {img: 'imgur8', matched: false},
+   {img: 'imgur8', matched: false},
+   {img: 'imgur9', matched: false},
+   {img: 'imgur9', matched: false},
+   {img: 'imgurl0', matched: false},
+   {img: 'imgurl0', matched: false}
    
 ];
 
 
 
+
+
+
 /*----- app's state (variables) -----*/
-//need to randomly sort array and floor off the decimals
-//gamestatus should be set to inplay, or win
 let gameStatus;
+let win;
 
 
 
 
 /*----- cached element references -----*/
-//I could assign each individual Div 1-20 a unique ID but it is better
-//to simply ID assign the underlying section/grid
-//need to find the guard  
+//divs need to be able to be interacted with via EventListener,
+
 
 
 
 /*----- event listeners -----*/
-// document.querySelector('section')
-// .addEventListener('click' , handleCardClick);
-
 
 
 
 
 /*----- functions -----*/
+
 initalize();
 
 function initalize() {
-    for (let i=0;i>200; i++) {
+    for (let i=0;i<board.length; i++) {
         const r1 = Math.floor(Math.random() * board.length);
         const r2 = Math.floor(Math.random() * board.length);
         const temp = board[r1];
@@ -50,22 +64,14 @@ function initalize() {
 
    
 
-    render()
+   
 }
 
 
 
-// function handleCardClick(evt) 
-// const card = document.querySelector('section>div')
-// if (evt.target1 && evt.target2 !== card);
-// return null;
-// else if (evt.target1 === evt.target2);
-//     card.push(evt)(confirmedMatches);
-//     else return null; 
 
 
     
 
 
-//When I get a correct guess I need to .push the pair out of the matches
-//array into a new array
+//
