18d755c993ff0ea8b960b90e340e2a83ce2df812
JoeyGrimes <107405472+JoeyGrimes@users.noreply.github.com>
2022-07-01 17:20:23 -0700

    Restructing HTML, and setting CSS

--- js/main.js
+++ js/main.js
@@ -2,17 +2,22 @@
 //Need to have an array of 1-20 with 10 possible matches
 //Better to have one long array rather than 10 two number arrays. 
 //Need to figure out how I can group. Whether by ID or color & textContent
-array = ['']
-confirmedMatches = ['']
-
+const board = [ 
+   {img: "https://st2.depositphotos.com/1803332/9245/v/950/depositphotos_92457980-stock-illustration-playing-cards-back.jpg"
+   {img: "https://st2.depositphotos.com/1803332/9245/v/950/depositphotos_92457980-stock-illustration-playing-cards-back.jpg"
+   , matched: false}, 
+   {img: 'imgurl2', matched: false}, 
+   {img: 'imgurl2', matched: false}, 
+   
+];
 
 
 
 /*----- app's state (variables) -----*/
 //need to randomly sort array and floor off the decimals
 //gamestatus should be set to inplay, or win
-gameStatus = null;
-guessedCards = ['']
+let gameStatus;
+
 
 
 
@@ -24,30 +29,39 @@ guessedCards = ['']
 
 
 /*----- event listeners -----*/
-document.querySelector('section')
-.addEventListener('click' handleCardClick);
+// document.querySelector('section')
+// .addEventListener('click' , handleCardClick);
+
 
 
 
 
 /*----- functions -----*/
-//To start the game
-initalize() {
-let cards
+initalize();
 
+function initalize() {
+    for (let i=0;i>200; i++) {
+        const r1 = Math.floor(Math.random() * board.length);
+        const r2 = Math.floor(Math.random() * board.length);
+        const temp = board[r1];
+        board[r1] = board[r2];   
+        board[r2] = temp;
+    }
 
+   
+
+    render()
 }
-//need to render out all previously defined IDs and Imgs
-render()
 
 
-function handleCardClick(evt)
-const card = document.querySelector('section>div')
-if (evt.target1 && evt.target2 !== card);
-return null;
-else if (evt.target1 === evt.target2);
-    card.push(evt)(confirmedMatches);
-    else return null; 
+
+// function handleCardClick(evt) 
+// const card = document.querySelector('section>div')
+// if (evt.target1 && evt.target2 !== card);
+// return null;
+// else if (evt.target1 === evt.target2);
+//     card.push(evt)(confirmedMatches);
+//     else return null; 
 
 
     
